7dc767bdb2a39bc41339d38d6659bed6a308b62b
das-palash-appsbee <plash.das@appsbee.com>
2020-09-17 16:33:40 +0530

    project named as shopify (#14)
    * location key added into info.plist
    
    * location key when in use added
    
    * possible fix for framework not found issue
    
    * regenarted xcworkspace
    
    * added dynamic flag
    
    * project named as shopify

---
+++ Shopify Demo/Shopify Demo/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  Shopify Demo
+//
+//  Created by Intempt on 29/08/20.
+//  Copyright © 2020 Intempt. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    //  MARK: - Application Launch -
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        /* ----------------------------------------
+         ** Initialize the cart controller and pre-
+         ** load any cached cart items.
+         */
+        UNUserNotificationCenter.current().delegate = PushManager.sharedInstance
+        PushManager.sharedInstance.registerForPushNotifications()
+        _ = CartController.shared
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        PushManager.sharedInstance.didRegisterForRemoteNotifications(deviceToken: deviceToken)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("didFailToRegisterForRemoteNotificationsWithError: \(error)")
+    }
+}
+
